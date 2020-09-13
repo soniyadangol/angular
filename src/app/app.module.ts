0b53207edc3e06e8b73e7587cf9122fcf4481d5b
Soniya <soniya@nepotech.com>
2020-09-13 22:57:44 +0545

    template driven form exercise part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -50,10 +52,13 @@ import { LoginComponent } from './login/login.component';
     MatToolbarModule,
     MatDialogModule,
     MatButtonModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   entryComponents: [
     LoginComponent
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {MatFormFieldControl} from '@angular/material';
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+	
+	contactForm: FormGroup;
+	feedback: Feedback;
+	contactType = ContactType;
 
-  constructor() { }
+	@ViewChild('feedbackForm') feedbackFormDirective;
+
+  constructor(
+  	private formBuilder: FormBuilder
+  ) {
+  		this.createForm();
+	}
 
   ngOnInit() {
   }
 
+  createForm() {
+  	this.contactForm = this.formBuilder.group({
+  		firstName: ['', Validators.required],
+  		lastName: ['', Validators.required],
+  		telNum: ['', Validators.required],
+  		email: ['', Validators.required],
+  		agree: false,
+  		contactType: 'None',
+  		message: ''
+  	})
+  }
+
+  onSubmit() {
+  	this.feedback = this.contactForm.value;
+  	console.log(this.feedback);
+  	this.contactForm.reset({
+  		firstName: '',
+		lastName: '',
+  		telNum: '',
+  		email: '',
+  		agree: false,
+  		contactType: 'None',
+  		message: ''
+  	});
+  	this.feedbackFormDirective.resetForm();
+  }
 }
